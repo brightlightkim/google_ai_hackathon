85dbeed96e4fa4977230918b4206508d3d5fa9ef
Kwanjung Cho <kwanjungcho@gmail.com>
2024-04-30 12:51:28 -0600

    Created

---
+++ backend/api/x_twits_lookup.js
@@ -0,0 +1,45 @@
+const needle = require('needle');
+const token = process.env.TWITTER_BEARER_TOKEN;
+const endpointURL = "https://api.twitter.com/2/tweets?ids=";
+
+async function getRequest() {
+
+    // These are the parameters for the API request
+    // specify Tweet IDs to fetch, and any additional fields that are required
+    // by default, only the Tweet ID and text are returned
+    const params = {
+        "ids": "1278747501642657792,1255542774432063488", // Edit Tweet IDs to look up
+        "tweet.fields": "lang,author_id", // Edit optional query parameters here
+        "user.fields": "created_at" // Edit optional query parameters here
+    }
+
+    // this is the HTTP header that adds bearer token authentication
+    const res = await needle('get', endpointURL, params, {
+        headers: {
+            "User-Agent": "v2TweetLookupJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.body) {
+        return res.body;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
+}
+
+(async () => {
+
+    try {
+        // Make request
+        const response = await getRequest();
+        console.dir(response, {
+            depth: null
+        });
+
+    } catch (e) {
+        console.log(e);
+        process.exit(-1);
+    }
+    process.exit();
+})();
