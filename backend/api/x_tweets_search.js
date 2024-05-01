7cb88b787da8d0e4bb4ac6b54b3742788a62badb
Kwanjung Cho <kwanjungcho@gmail.com>
2024-04-30 19:59:28 -0600

    using recent search request in x api v2 instead of tweet lookup

---
+++ backend/api/x_tweets_search.js
@@ -0,0 +1,51 @@
+// Search for Tweets within the past seven days
+// https://developer.twitter.com/en/docs/twitter-api/tweets/search/quick-start/recent-search
+
+const needle = require('needle');
+
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const token = process.env.X_BEARER_TOKEN;
+
+const endpointUrl = "https://api.twitter.com/2/tweets/search/recent";
+
+async function getRequest() {
+
+    // Edit query parameters below
+    // specify a search query, and any additional fields that are required
+    // by default, only the Tweet ID and text fields are returned
+    const params = {
+        'query': 'from:twitterdev -is:retweet',
+        'tweet.fields': 'author_id'
+    }
+
+    const res = await needle('get', endpointUrl, params, {
+        headers: {
+            "User-Agent": "v2RecentSearchJS",
+            "authorization": `Bearer ${token}`
+        }
+    })
+
+    if (res.body) {
+        return res.body;
+    } else {
+        throw new Error('Unsuccessful request');
+    }
+}
+
+(async () => {
+
+    try {
+        // Make request
+        const response = await getRequest();
+        console.dir(response, {
+            depth: null
+        });
+
+    } catch (e) {
+        console.log(e);
+        process.exit(-1);
+    }
+    process.exit();
+})();
